a866ea607353a675b6a4840bf87af27c30839f2f
malkiro <malkiroshika3@gmail.com>
2023-05-12 15:56:01 +0530

    created product ui

---
+++ src/com/devstack/pos/controller/ProductMainPageController.java
@@ -0,0 +1,11 @@
+package com.devstack.pos.controller;
+
+import javafx.event.ActionEvent;
+
+public class ProductMainPageController {
+    public void btnBackToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+    }
+}
