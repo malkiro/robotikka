c49039dc4812dce445e71434503392a9e6bc3e1a
malkiro <malkiroshika3@gmail.com>
2023-05-12 11:37:32 +0530

    created customer save process

--- src/com/devstack/pos/dao/DatabaseAccessCode.java
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -4,8 +4,10 @@ import com.devstack.pos.util.PasswordManager;
 import dto.UserDto;
 
 import java.sql.*;
+import java.util.List;
 
 public class DatabaseAccessCode {
+    //==========User management================
     public static boolean createUser(String email, String password) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
@@ -34,4 +36,42 @@ public class DatabaseAccessCode {
         }
             return null;
     }
+    //==========User management================
+
+
+
+
+    //==========Customer management================
+    public static boolean createCustomer(String email, String name, String contact, double salary) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root","1234");
+        String sql="INSERT INTO customer VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, name);
+        preparedStatement.setString(3, contact);
+        preparedStatement.setDouble(4, salary);
+        return preparedStatement.executeUpdate()>0;
+    }
+
+    public static boolean updateCustomer(String email, String name, String contact, double salary){
+        return false;
+    }
+
+    public static CustomerDto findCustomer(String email){
+        return null;
+    }
+
+    public static boolean deleteCustomer(String email){
+        return false;
+    }
+
+    public static List<CustomerDto> findAllCustomers(){
+        return null;
+    }
+    public static List<CustomerDto> searchCustomers(String searchText){
+        return null;
+    }
+    //==========Customer management================
 }
