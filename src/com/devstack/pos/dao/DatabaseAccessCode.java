267f3978a98ab3931a4402cf90594d8dabaf5eed
malkiro <malkiroshika3@gmail.com>
2023-05-12 10:44:02 +0530

    added the database access code class

---
+++ src/com/devstack/pos/dao/DatabaseAccessCode.java
@@ -0,0 +1,37 @@
+package com.devstack.pos.dao;
+
+import com.devstack.pos.util.PasswordManager;
+import dto.UserDto;
+
+import java.sql.*;
+
+public class DatabaseAccessCode {
+    public static boolean createUser(String email, String password) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root","1234");
+        String sql="INSERT INTO user VALUES (?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, PasswordManager.encryptPassword(password));
+        return preparedStatement.executeUpdate()>0;
+    }
+
+    public static UserDto findUser(String email) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
+                "root", "1234");
+        String sql = "SELECT * FROM user WHERE email=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, email);
+
+        ResultSet set = preparedStatement.executeQuery();
+        if (set.next()){ //set.next() method eka true wenawa kiyanne user knk awith
+            return new UserDto(
+                    set.getString(1),
+                    set.getString(2)
+            );
+        }
+            return null;
+    }
+}
